bbdada9b55f555efb998437d11962db34abf19ee
shock_treatment <shock_treatment@icloud.com>
2023-04-02 18:43:37 +0300

    team

---
+++ src/menu-phone.js
@@ -0,0 +1,14 @@
+// (() => {
+//   const refs = {
+//     openMenuBtn: document.querySelector('[data-menu-phone-open]'),
+//     closeMenuBtn: document.querySelector('[data-menu-phone-close]'),
+//     menu: document.querySelector('[data-phone-menu]'),
+//   };
+
+//   refs.openMenuBtn.addEventListener('click', toggleModal);
+//   refs.closeMenuBtn.addEventListener('click', toggleModal);
+
+//   function toggleModal() {
+//     refs.menu.classList.toggle('is-open');
+//   }
+// })();
---
+++ src/video.js
@@ -0,0 +1,4 @@
+document.querySelector(".video__button").onclick = function(){
+    this.style.display = "none";
+    document.querySelector(".video__overlay").style.display = "none";
+}
